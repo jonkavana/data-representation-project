c7fcb6d911ccb19b621613723000077ba9801585
jonkavana <G00387868@gmit.ie>
2021-12-30 09:44:23 +0000

    testing to ensure that the interface is working as expected completed.

--- server.py
+++ server.py
@@ -17,13 +17,60 @@ def login():
 def getUser():
     return "Served by the getUser function"
 
-@app.route('/user/<int:id>')
-def getById(id):
-    return "Served by the getById function with id = "+str(id) 
+# Get All records for students 
+@app.route('/student')
+def getAll():
+    return jsonify([])
 
-@app.route('/user', methods=['POST'])
+# Get user by id
+@app.route('/student/<int:id>')
+def getById(id):
+    return jsonify({}) 
+    #return "Served by the getById function with id = "+str(id) 
+
+# code that was run in order to return create testing correctly
+# curl -X POST -d "{\"id\":10, \"name\": \"test\", \"age\":89}" -H Content-Type:application/json http://127.0.0.1:5000/student
+
+# Create a user
+@app.route('/student', methods=['POST'])
 def createUser():
-    return "Served by the createUser function"
+    if not request.json:
+        abort(400)
+    # return "Served by the createUser function"
+    studen = {
+        "id": request.json["id"],
+        "name": request.json["name"],
+        "age": request.json["age"]
+    }
+    return jsonify({})
+
+# code that was run in order to return update testing correctly
+# λ curl -X PUT -d "{\"name\": \"test2\", \"age\":56}" -H Content-Type:application/json http://127.0.0.1:5000/student/123
+
+# Update a id
+@app.route('/student/<int:id>', methods=['PUT'])
+def update(id):
+    foundBooks = []
+    if len(foundBooks) == 0:
+        return jsonify({}), 404
+    currentBook = foundBooks[0]
+    if 'name' in request.json:
+        currentBook['name'] = request.json['name']
+    if 'age' in request.json:
+        currentBook['age'] = request.json['age']
+    return jsonify(currentBook)
+
+# code that was run in order to return DELETE testing correctly
+# curl -X DELETE http://127.0.0.1:5000/student/123
+
+# delete by id
+@app.route('/student/<int:id>', methods=['DELETE'])
+def delete(id):
+    return jsonify({"done":True})
+
+
+
+
 
 @app.route("/demo_url_for")
 def demoUrlFor():
@@ -37,5 +84,4 @@ def demoRequest():
     return request.method
 
 if __name__ == "__main__":
-    print("in if")
     app.run(debug=True)
